0bebba8c436a80d6598f85b183f232fdc65bd252
BALA RAVI <bala444learning@gmail.com>
2026-05-25 23:23:45 +0530

    find the largest of two numbers

---
+++ days/day-01/code/04_largest_of_two.py
@@ -0,0 +1,9 @@
+# Problem 4 - Largest of Two Numbers
+# Day 01 - Python Fundamentals
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print(a, "is largest")
+else:
+    print(b, "is largest")
