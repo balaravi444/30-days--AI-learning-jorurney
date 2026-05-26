cbf90139e9d7d8519d5513cc1ed0f11a149abea9
BALA RAVI <bala444learning@gmail.com>
2026-05-26 17:45:28 +0530

    03_average_of_three.py

---
+++ days/day-02/code/03_average_of_three.py
@@ -0,0 +1,8 @@
+# Problem 3 - Calculate Avearge of 3 Numbers
+# Day 02 - Operators & Conditions
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+average = (a + b + c) / 3
+print("Average =", average)
