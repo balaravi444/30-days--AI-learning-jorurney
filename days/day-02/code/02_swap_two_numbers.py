bd4b4f2eebc850b2e15abdb422c5d485bfe421c9
BALA RAVI <bala444learning@gmail.com>
2026-05-26 17:42:10 +0530

    02_swap_two_numbers.py

---
+++ days/day-02/code/02_swap_two_numbers.py
@@ -0,0 +1,11 @@
+# Problem 2 - Swap Two Numbers
+# Day 02 - Operators & Conditions
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+temp = a
+a = b
+b = temp
+print("After swapping:")
+print("a =", a)
+print("b =", b)
