61646647c1447b682afdab9230a7d7d5f916ffe8
BALA RAVI <bala444learning@gmail.com>
2026-05-27 22:09:21 +0530

    07_marks_validation.py

---
+++ days/day-03/code/07_marks_validation.py
@@ -0,0 +1,8 @@
+# Problem 7 - Marks validation Program
+# Day 03 - Control Flow
+
+marks = int(input("Enter marks: "))
+if marks > 100 or marks < 0:
+  print("Invalid marks")
+else:
+  print("Valid marks")
