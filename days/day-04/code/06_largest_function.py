d9429b49ab194dabf68666b11811a16e7cf6d200
BALA RAVI <bala444learning@gmail.com>
2026-05-28 01:35:50 +0530

    06_largest_function.py

---
+++ days/day-04/code/06_largest_function.py
@@ -0,0 +1,10 @@
+# Problem 6 - Function to Find Largest Number
+# Day 04 - Functions & Variable Scope
+
+def largest(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print("Largest =", largest(25, 18))
