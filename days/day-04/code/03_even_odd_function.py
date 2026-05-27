af99d88d8f7ba90a3c05b78c78edf62c9cf60db5
BALA RAVI <bala444learning@gmail.com>
2026-05-28 01:31:45 +0530

    03_even_odd_function.py

---
+++ days/day-04/code/03_even_odd_function.py
@@ -0,0 +1,9 @@
+# Problem 2 - Function to Check Even or Odd
+# Day 04 - Functions & Variable Scope
+
+def check_even_odd(num):
+  if num % 2 == 0:
+    return "even"
+  else:
+    return "odd"
+print(check_even_odd(8))
