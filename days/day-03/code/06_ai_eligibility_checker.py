365c17f20c2d594389139e7c4cf8d026b76ce59f
BALA RAVI <bala444learning@gmail.com>
2026-05-27 22:06:34 +0530

    06_ai_eligibility_checker.py

---
+++ days/day-03/code/06_ai_eligibility_checker.py
@@ -0,0 +1,9 @@
+# Problem 6 - AI Program Eligibility Checker
+# Day 03 - Control Flow
+
+marks = int(input("Enter marks: "))
+age = int(input("Enter age: "))
+if marks >= 75 and age >= 18:
+  print("Eligible for AI Program")
+else:
+  print("Not Eligible for AI Program")
