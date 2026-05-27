ea172db89cf6a83967333fcce4ff91c2d438aac7
BALA RAVI <bala444learning@gmail.com>
2026-05-27 22:02:59 +0530

    05_login_system.py

---
+++ days/day-03/code/05_login_system.py
@@ -0,0 +1,10 @@
+# Problem 5 - Login system
+# Day 03 - Control Flow
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+if username == "bala" and password == "4444":
+  print("Login Successful")
+else:
+  print("Invalid details")
+  
