b7b9a0e807c160c589939de28ceb954b87745117
BALA RAVI <bala444learning@gmail.com>
2026-05-28 19:02:58 +0530

    06_filter_even_numbers.py

---
+++ days/day-07/code/06_filter_even_numbers.py
@@ -0,0 +1,6 @@
+# Problem 6 - Filtering Even Numbers
+# Day 07 - Sets, Arrays & List Comprehensions
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+evens = [n for n in numbers if n % 2 == 0]
+print("Even numbers:", evens)
