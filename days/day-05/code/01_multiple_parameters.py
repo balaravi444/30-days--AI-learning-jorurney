103011059f106b42d769226a9be358430658debd
BALA RAVI <bala444learning@gmail.com>
2026-05-28 18:48:09 +0530

    01_multiple_parameters.py

---
+++ days/day-05/code/01_multiple_parameters.py
@@ -0,0 +1,7 @@
+# Problem 1 - Function with Multiple Parameters
+# Day 05 - Advanced Functions & Recursion
+
+def add_numbers(a, b, c):
+    return a + b + c
+
+print("Sum =", add_numbers(10, 20, 30))
