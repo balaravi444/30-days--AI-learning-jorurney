ae448775c5a7f39d75d5d026ed50dd26ed5655d3
BALA RAVI <bala444learning@gmail.com>
2026-05-29 11:38:04 +0530

    01_counter.py

---
+++ days/day-08/code/01_counter.py
@@ -0,0 +1,7 @@
+# Problem 1 - Counting Frequency Using Counter
+# Day 08 - Collections Module
+from collection import Counter
+
+text = input("Enter a string: ")
+frequency = Counter(text)
+print("Character frequency:", frequency)
